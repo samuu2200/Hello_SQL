9a8cca90abbf1594627bdd441ea6d85ce499c44d
samdev2200 <samuel2200@hotmail.es>
2025-03-09 13:02:39 +0100

    Clases de writing en sql

---
+++ 02_Writing/01_insert.sql
@@ -0,0 +1,8 @@
+-- Inserta un registro con identificador, nombre y apellido en la tabla "users"
+INSERT INTO users (user_id, name, surname) VALUES (8, 'María', 'López')
+
+-- Inserta un registro con nombre y apellido en la tabla "users"
+INSERT INTO users (name, surname) VALUES ('Paco', 'Pérez')
+
+-- Inserta un registro con identificador no correlativo, nombre y apellido en la tabla "users"
+INSERT INTO users (user_id, name, surname) VALUES (11, 'El', 'Merma')
---
+++ 02_Writing/03_delete.sql
@@ -0,0 +1,2 @@
+-- Elimina el registro de la tabla "users" con identificador igual a 11
+DELETE FROM users WHERE user_id = 11;
